8f706ceb559b996170c9f51418f77af992a463a1
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-13 14:16:11 +0200

    hello.py

---
+++ Python/Alapok/hello.py
@@ -0,0 +1,19 @@
+print("Hello world!")
+# Ez itt egy újabb sort ír ki!
+print('Heló világ!')
+
+"""
+Több soros komment
+"""
+
+'''
+Ez is
+több soros komment
+'''
+
+# Ez is egy komment!
+# És ez is az!
+
+print("I'm Norbert!")
+print('"I\'m Norbert!"')
+print('"Nagyon szeretjük a pörgő macskát!" Alex')
