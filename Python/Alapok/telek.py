0e165eb08389976251ca33d4b51130d839e0d0cf
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-18 11:55:53 +0200

    telek vegso

--- Python/Alapok/telek.py
+++ Python/Alapok/telek.py
@@ -1,7 +1,11 @@
-a = 45
-b = 35
+# Beolvasás
+a = int(input("Hosszúsága: "))
+b = int(input("Szélesség: "))
 
+# Feldolgozás
 kerites = 2*(a+b) - 5
 
+# Kiírás
 # print("A kerítés mérete: ", kerites, "m.", sep="")
-print("A kerítés mérete:", str(kerites) + "m.")
+# print("A kerítés mérete:", str(kerites) + "m.")
+print(f"A kerítés mérete: {kerites}m.")
