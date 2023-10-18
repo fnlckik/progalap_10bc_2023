7a612f41152f978bfb46313f6d6f835f4b21d550
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-18 12:26:52 +0200

    2050

---
+++ Python/Alapok/2050.py
@@ -0,0 +1,10 @@
+# Beolvasás
+# aktualis = int(input("Milyen évet írunk?"))
+aktualis = 2023
+kor = int(input("Hány éves vagy? "))
+
+# Feldolgozás
+jovo = kor + 2050 - aktualis
+
+# Kiírás
+print(f"2050-ben {jovo} éves leszel!")
