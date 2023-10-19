bf2ced8bc91bba36e597ac1772dbdb85002cb017
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-19 13:27:38 +0200

    kiskutya.py

---
+++ Python/Alapok/kiskutya.py
@@ -0,0 +1,11 @@
+# Beolvasás
+k = int(input("Hány kutya van most? "))
+
+# Feldolgozás
+x = k + 1
+
+# Kiírás
+print("Új kiskutya született!")
+# print(x, "kiskutya")
+# print(str(x) + " kiskutya")
+print(f"{x} kiskutya")
---
+++ Python/Alapok/telek.py
@@ -0,0 +1,11 @@
+# Beolvasás
+a = 45
+b = 35
+kapu = 5
+
+# Feldolgozás
+kerulet = 2 * (a + b)
+kerites = kerulet - kapu
+
+# Kiiírás
+print(f"A kerítés hossza: {kerites}m.")
