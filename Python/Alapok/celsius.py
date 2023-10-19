dc7a04631a8055157cdefc18bc0a73589fc227b1
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-19 10:04:06 +0200

    celsius.py

---
+++ Python/Alapok/celsius.py
@@ -0,0 +1,9 @@
+# Beolvasás
+celsius = float(input("Celsius-fok: "))
+
+# Feldolgozás
+fahrenheit = 9/5 * celsius + 32
+
+# Kiirás
+# print("Fahrenheit-be átváltva:", fahrenheit)
+print(f"Fahrenheit-be átváltva: {fahrenheit}")
