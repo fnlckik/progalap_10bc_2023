84f602c8ee45906e2764b97f4df7c370dc4ecd52
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-20 14:06:15 +0200

    ido.py

---
+++ Python/Alapok/ido.py
@@ -0,0 +1,12 @@
+# Be
+ora = input("Óra: ")
+perc = input("Perc: ")
+mperc = input("Másodperc: ")
+
+# Feld
+
+# Ki
+print(ora, perc, mperc, sep=":")
+print(ora + ":" + perc + ":" + mperc)
+print(f"{ora}:{perc}:{mperc}")
+
