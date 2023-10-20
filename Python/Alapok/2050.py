0902fab3f82e6beb5286380f8b45eb98b14c6289
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-20 14:15:44 +0200

    2050.py

---
+++ Python/Alapok/2050.py
@@ -0,0 +1,10 @@
+# Be
+aktualis_kor = int(input("Életkor: ")) # 16
+aktualis_ev = int(input("Aktuális év: ")) # 2023
+
+# Feld
+# 2050 - 2023 + 16 == 43
+kesobbi_kor = 2050 - aktualis_ev + aktualis_kor
+
+# Ki
+print(kesobbi_kor)
