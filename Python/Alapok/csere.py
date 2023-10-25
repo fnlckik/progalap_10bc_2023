1e7e14443dda1465f9881a8b39c8c518d43776f3
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-25 08:22:04 +0200

    csere.py

---
+++ Python/Alapok/csere.py
@@ -0,0 +1,14 @@
+# Beolvasás
+a = int(input("1. sorszám: "))
+b = int(input("2. sorszám: "))
+print("Eredetileg:", a, b)
+
+# a: 3
+# b: 7
+# Feldolgozás
+seged = a
+a = b
+b = seged
+
+# Kiírás
+print("Fordítva:", a, b)
