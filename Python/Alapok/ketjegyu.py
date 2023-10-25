683c9e0303797db86834e3d0e78d7a141f6ed4d5
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-25 08:09:10 +0200

    ketjegyu.py

--- Python/Alapok/ketjegyu.py
+++ Python/Alapok/ketjegyu.py
@@ -1,2 +1,13 @@
+# 57 => 75
+
+# Beolvasás
+n = int(input("n: "))
+
 # Feld
-e = n % 10
+e = n % 10 #7
+t = n // 10 #5
+# eredmeny = str(e) + str(t) # "75"
+eredmeny = 10*e + t
+
+# Kiírás
+print("Fordítva:", eredmeny)
