7cfe25baef89e9a41a9467d3baf764bd16bc7814
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-25 10:53:53 +0200

    hetnapja.py

--- Python/Elágazás/hetnapja.py
+++ Python/Elágazás/hetnapja.py
@@ -2,19 +2,29 @@
 n = int(input("Hét napja: "))
 
 # Feldolgozás
+# if n == 1:
+#     hetnapja = "Hétfő"
+# else:
+#     if n == 2:
+#         hetnapja = "Kedd"
+#     else:
+#         if n == 3:
+#             hetnapja = "Szerda"
+#         else:
+#             if n == 4:
+#                 hetnapja = "Csütörtök"
+#             else:
+#                 hetnapja = "Péntek"
 if n == 1:
     hetnapja = "Hétfő"
+elif n == 2:
+    hetnapja = "Kedd"
+elif n == 3:
+    hetnapja = "Szerda"
+elif n == 4:
+    hetnapja = "Csütörtök"
 else:
-    if n == 2:
-        hetnapja = "Kedd"
-    else:
-        if n == 3:
-            hetnapja = "Szerda"
-        else:
-            if n == 4:
-                hetnapja = "Csütörtök"
-            else:
-                hetnapja = "Péntek"
+    hetnapja = "Péntek"
 
 # Kiírás
 print("Ez a nap:", hetnapja)
