cbf5589f4c7d145d4de6bc83e813ad23a4a8d6d7
farkas.norbert <farkas.norbert@ckik.hu>
2023-10-27 13:11:24 +0200

    hetnapja.py (szep mo)

--- Python/Elágazások/hetnapja.py
+++ Python/Elágazások/hetnapja.py
@@ -6,22 +6,35 @@ if nap < 1 or nap > 7:
     print("Nem jó! A hét napja 1 és 7 között legyen!")
     exit()
 
+# if nap == 1:
+#     hetnapja = "Hétfő"
+# else:
+#     if nap == 2:
+#         hetnapja = "Kedd"
+#     else:
+#         if nap == 3:
+#             hetnapja = "Szerda"
+#         else:
+#             if nap == 4:
+#                 hetnapja = "Csütörtök"
+#             else:
+#                 if nap == 5:
+#                     hetnapja = "Péntek"
+#                 else:
+#                     hetnapja = "Hétvége"
+
 if nap == 1:
     hetnapja = "Hétfő"
+elif nap == 2:
+    hetnapja = "Kedd"
+elif nap == 3:
+    hetnapja = "Szerda"
+elif nap == 4:
+    hetnapja = "Csütörtök"
+elif nap == 5:
+    hetnapja = "Péntek"
 else:
-    if nap == 2:
-        hetnapja = "Kedd"
-    else:
-        if nap == 3:
-            hetnapja = "Szerda"
-        else:
-            if nap == 4:
-                hetnapja = "Csütörtök"
-            else:
-                if nap == 5:
-                    hetnapja = "Péntek"
-                else:
-                    hetnapja = "Hétvége"
+    hetnapja = "Hétvége"
 
 # Kiírás
 print(hetnapja)
