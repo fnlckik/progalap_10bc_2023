998298c17452260be1165d293e65693e21f4118a
farkas.norbert <farkas.norbert@ckik.hu>
2023-11-09 10:21:58 +0100

    hello_n.py

---
+++ Python/Ciklus/hello_n.py
@@ -0,0 +1,7 @@
+n = int(input("Hányszor? "))
+
+for i in range(n):
+    # print(f"{i+1}. Hello Világ!")
+    # print(i+1, ". Hello Világ!", sep="")
+    print(str(i+1) + ". Hello Világ!")
+
---
+++ Python/Ciklus/hello_n2.py
@@ -0,0 +1,13 @@
+n = int(input("Hányszor? "))
+
+i = 0
+while i<n:
+    i += 1
+    print(f"{i}. Hello Világ!")
+
+'''
+i = 1
+while i<=n:
+    print(f"{i}. Hello Világ!")
+    i += 1
+'''
