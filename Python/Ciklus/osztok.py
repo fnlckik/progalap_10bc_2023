0db28a1064b67c7e81c7e6ba7dd4bdedc2fe0b4f
farkas.norbert <farkas.norbert@ckik.hu>
2023-11-15 08:30:19 +0100

    osztok.py

---
+++ Python/Ciklus/osztok.py
@@ -0,0 +1,20 @@
+# Kapunk egy egész számot!
+# Írjuk ki a pozitív egész osztóit!
+
+# Minta:
+# n: 6
+# 1 2 3 6
+
+# Minta:
+# n: 24
+# 1 2 3 4 6 8 12 24
+
+# Beolvasás
+n = int(input("n: "))
+
+# Feldolgozás + Kiírás
+print("A szám osztói:")
+for i in range(1, n+1):
+    if n % i == 0:
+        print(i, end=" ")
+
