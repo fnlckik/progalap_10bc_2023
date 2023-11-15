6278ab2915dcf71bf0de063e06737f048ae3a84a
farkas.norbert <farkas.norbert@ckik.hu>
2023-11-15 08:21:25 +0100

    faktorialis.py

---
+++ Python/Ciklus/faktorialis.py
@@ -0,0 +1,27 @@
+# n! => 1*2*3*4*...*(n-1)*n
+# 5! => 1*2*3*4*5 => 120
+# 1! => 1
+# 2! => 1*2 => 2
+# 3! => 1*2*3 => 6
+# 4! => 1*2*3*4 => 24
+# 0! => 1
+
+# Olvass be egy n poz. egész számot
+# Írd ki n! értékét
+
+# Beolvasás
+n = int(input("n: "))
+
+# Feldolgozás
+# fakt = 1
+# for i in range(1, n+1):
+#     fakt *= i
+
+fakt = 1
+i = 1
+while i <= n:
+    fakt *= i
+    i += 1
+
+# Kiírás
+print(n, "faktoriális:", fakt)
