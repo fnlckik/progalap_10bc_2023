168c605c74ba9d2a64cda4fc6420cee624d8c887
farkas.norbert <farkas.norbert@ckik.hu>
2023-11-20 14:21:12 +0100

    primszam_Balazs magyarázat

--- Python/Ciklus/primszam_Balazs.py
+++ Python/Ciklus/primszam_Balazs.py
@@ -3,13 +3,15 @@
 
 p = int(input("p: "))
 
+# Keresek 1-nél nagyobb p-nél kisebb osztót (kezdetben nincs)
 oszto = 0
 i = 2
-while i < p and oszto == 0:
+while i < p and oszto == 0: # csak addig megyek amig nincs oszto
     if p % i == 0:
         oszto = i
     i += 1
 
+# Ha nem találtam 1-nél nagyobb p-nél kisebb osztót akkor prím
 if oszto == 0:
     print("Prím")
 else:
