6e123105ec52cc6954eb9c549adc6e6df2334be0
farkas.norbert <farkas.norbert@ckik.hu>
2023-11-24 10:58:22 +0100

    osszeg_paratlan.py

--- Python/Ciklus/osszeg_paratlan.py
+++ Python/Ciklus/osszeg_paratlan.py
@@ -6,8 +6,29 @@
 # Beolvasás
 n = int(input("n: "))
 
-# Feldolgozás
-# osszeg = ???
+# Feldolgozás - Ádám
+# osszeg = 0
+# for i in range(1, n*2):
+#     if i % 2 != 0:
+#         osszeg += i
+
+# Feldolgozás - Máté
+'''
+osszeg = 0
+ertek = 1
+# print(osszeg, end="")
+for i in range(n):
+    osszeg += ertek
+    print(f"{osszeg-ertek} + {ertek} = {osszeg}")
+    ertek += 2
+print()
+'''
+
+# Feldolgozás - Kata
+# osszeg = 0
+# for i in range(1, n*2, 2):
+#    osszeg += i
+
 
 # Kiírás
 print("Összeg:", osszeg)
