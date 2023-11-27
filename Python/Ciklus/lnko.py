9b64483b211b82a4c67bd895e633deff9a8e4c96
farkas.norbert <farkas.norbert@ckik.hu>
2023-11-27 14:16:28 +0100

    lnko

---
+++ Python/Ciklus/lnko.py
@@ -0,0 +1,17 @@
+# lnko: legnagyobb közös osztó
+
+# (4, 6) = 2
+# (8, 12) = 4
+
+a = int(input("a: "))
+b = int(input("b: "))
+
+c, d = a, b
+while c != d:
+    if c > d:
+        c -= d
+    else:
+        d -= c
+    print(f"({c}, {d})")
+
+print(f"({a}, {b}) = {c}")
