c3ac32f292d2e0c8d1cfcf8df1ff61fe0b5e21fc
farkas.norbert <farkas.norbert@ckik.hu>
2023-12-11 09:13:47 +0100

    nevek.py, parok.py

---
+++ Python/Listák/nevek.py
@@ -0,0 +1,7 @@
+nevek = ["Anna", "Bea", "Cintia", "Dani" ,"Erik"]
+
+# nevek[2] == "Cintia"
+
+n = len(nevek)
+for i in range(n):
+    print(f"{i+1}. {nevek[i]}")
---
+++ Python/Listák/parok.py
@@ -0,0 +1,7 @@
+x = [3, 5, 2, 7, 10, 4]
+
+# x[3] == 7
+n = len(x)
+
+for i in range(n-1):
+    print(f"{x[i]} - {x[i+1]}")
