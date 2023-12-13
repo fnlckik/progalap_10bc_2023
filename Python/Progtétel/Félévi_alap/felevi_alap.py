c490fc9cf5a51941055cf928b5fc19c89909955d
farkas.norbert <farkas.norbert@ckik.hu>
2023-12-13 12:26:48 +0100

    felevi F1-F3

--- Python/Progtétel/Félévi_alap/felevi_alap.py
+++ Python/Progtétel/Félévi_alap/felevi_alap.py
@@ -22,16 +22,32 @@ szakmai = [
 jegyek = [3, 1, 2, 4, 5, 4, 2, 5, 5, 4, 1, 4]
 
 #------------------------------------------------------------------
-
+n = len(jegyek)
 
 # F1
-
+# Összegzés (sorozatszámítás) tétel
+osszeg = 0
+for i in range(n):
+    osszeg += jegyek[i]
+atlag = osszeg / n
+print(f"1. Átlag: {round(atlag, 2)}")
 
 # F2
-
+# Megszámolás
+db = 0
+for i in range(n):
+    if jegyek[i] > 1:
+        db += 1
+arany = db / n * 100
+print(f"2. Teljesített tárgyak aránya: {round(arany, 2)}%")
 
 # F3
-
+# Megszámolás
+db = 0
+for i in range(n):
+    if jegyek[i] == 1 and szakmai[i]:
+        db += 1
+print(f"3. Bukott szakmai tárgyak száma: {db}")
 
 # F4
 
