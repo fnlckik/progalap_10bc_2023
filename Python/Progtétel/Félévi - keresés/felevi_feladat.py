0799d594c7b1bc347496a9bdcd1d1d491fecf6b0
farkas.norbert <farkas.norbert@ckik.hu>
2024-01-12 13:34:53 +0100

    Félévi - keresés (feladatok)

---
+++ Python/Progtétel/Félévi - keresés/felevi_feladat.py
@@ -0,0 +1,44 @@
+# Tantárgyak listája
+targyak = [
+    "Fizika", "Matek",
+    "Testnevelés", "Irodalom",
+    "Angol", "Történelem",
+    "Progalap", "IKT",
+    "Digitális kultúra", "ITA",
+    "Webprog", "Adatbázis"
+]
+
+# Szakmai tantárgy-e?
+szakmai = [
+    False, False,
+    False, False,
+    False, False,
+    True, True,
+    False, True,
+    True, True,
+]
+
+# Félév végi jegyek
+jegyek = [3, 1, 2, 4, 5, 4, 2, 5, 5, 4, 1, 4]
+
+#------------------------------------------------------------------
+n = len(jegyek)
+
+# F1: Igaz-e, hogy minden tárgyból átment? (nem igaz)
+
+
+# F2: Melyik a leghosszabb nevű tantárgy? (Digitális kultúra)
+
+
+# F3: Van-e olyan tantárgy, amiből ugyanolyan jegyet szerzett, mint irodalomból?
+# Feltehető, hogy van "Irodalom" nevű tantárgy!
+# Van: (Történelem)
+
+
+# F4: Van-e olyan szakmai tárgy, amiből 3-as jegynél rosszabbat kapott?
+# Ha igen, akkor add meg az utolsó ilyen tárgy nevét! (Webprog)
+
+
+# F5: Van-e két egymást követő tantárgy, ami ugyanazzal a betűvel kezdődik?
+# Ha igen, add meg a nevüket! (most nincs, de próbáld ki olyan listával is, amikor van)
+
