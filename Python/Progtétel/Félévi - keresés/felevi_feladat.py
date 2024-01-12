5cc679bc5eef489a5e21adc07315a50e67b82e46
farkas.norbert <farkas.norbert@ckik.hu>
2024-01-12 14:15:16 +0100

    felevi - keresés, eldöntés

--- Python/Progtétel/Félévi - keresés/felevi_feladat.py
+++ Python/Progtétel/Félévi - keresés/felevi_feladat.py
@@ -25,15 +25,36 @@ jegyek = [3, 1, 2, 4, 5, 4, 2, 5, 5, 4, 1, 4]
 n = len(jegyek)
 
 # F1: Igaz-e, hogy minden tárgyból átment? (nem igaz)
-
+# Eldönt: Van-e tárgy, amiből megbukott?
+i = 0
+while i < n and not(jegyek[i] == 1):
+    i += 1
+if i < n:
+    print("1. Nem igaz, hogy minden tárgyból átment!")
+else:
+    print("1. Minden tárgyból átment!")
 
 # F2: Melyik a leghosszabb nevű tantárgy? (Digitális kultúra)
-
+maxi = 0
+for i in range(1, n):
+    if len(targyak[i]) > len(targyak[maxi]):
+        maxi = i
+print("2. A leghosszabb nevű tárgy:", targyak[maxi])
 
 # F3: Van-e olyan tantárgy, amiből ugyanolyan jegyet szerzett, mint irodalomból?
 # Feltehető, hogy van "Irodalom" nevű tantárgy!
 # Van: (Történelem)
-
+# 1. lépés: válasszuk ki az irodalom indexét!
+i = 0
+while not(targyak[i] == "Irodalom"):
+    i += 1
+irodalom = i
+# 2. lépés: keressünk egy másik tárgyat, amiből szintén ezt a jegyet kapta
+i = 0
+while i < n and not(jegyek[i] == jegyek[irodalom] and i != irodalom):
+    i += 1
+if i < n:
+    print("3. Ugyanolyan jegy, mint irodalomból:", targyak[i])
 
 # F4: Van-e olyan szakmai tárgy, amiből 3-as jegynél rosszabbat kapott?
 # Ha igen, akkor add meg az utolsó ilyen tárgy nevét! (Webprog)
