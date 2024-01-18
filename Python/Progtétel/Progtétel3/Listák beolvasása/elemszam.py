b473286639af084c816600ef55d61391929481c0
farkas.norbert <farkas.norbert@ckik.hu>
2024-01-18 13:26:16 +0100

    elemszam.py

---
+++ Python/Progtétel/Progtétel3/Listák beolvasása/elemszam.py
@@ -0,0 +1,13 @@
+# szamok = [2, 7, 8, -1, 7, 12]
+
+# Olvass be N egész számot
+# egy listába!
+# Először: 5 db
+
+n = int(input())
+
+szamok = []
+for i in range(n):
+    szam = int(input())
+    szamok.append(szam)
+print(szamok)
