87e6bb5d2d260b37aafc25b40022cc1b236b2801
farkas.norbert <farkas.norbert@ckik.hu>
2024-01-18 10:37:27 +0100

    elemszam.py

--- Python/Progtétel/Progtétel3/Listák beolvasása/elemszam.py
+++ Python/Progtétel/Progtétel3/Listák beolvasása/elemszam.py
@@ -2,9 +2,22 @@
 
 # Olvass be és tárolj el
 # n darab számot egy listába!
+n = int(input())
 szamok = []
-for i in range(5):
-    bemenet = int(input("szam: "))
+for i in range(n):
+    bemenet = int(input())
     szamok.append(bemenet)
 print(szamok)
 
+# Összeg
+s = 0
+for i in range(n):
+    s += szamok[i]
+print(s)
+
+# Négyzetek
+y = []
+for i in range(n):
+    negyzet = szamok[i]*szamok[i]
+    y.append(negyzet)
+print(y)
