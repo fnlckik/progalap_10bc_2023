c6469ee6b68398867b180ec3d915c5e237417f19
farkas.norbert <farkas.norbert@ckik.hu>
2024-01-25 13:24:23 +0100

    atlagfelettiek.py

---
+++ Python/Mester/Programozási tételek összeépítése/2 - 3. Átlag feletti szennyezettség/atlagfelettiek.py
@@ -0,0 +1,15 @@
+# Be
+n = int(input())
+reggeliek, estiek = [], []
+for i in range(n):
+    sor = input().split() # ["100", "120"]
+    reggeli = int(sor[0])
+    reggeliek.append(reggeli)
+    esti = int(sor[1])
+    estiek.append(esti)
+
+# print(reggeliek)
+# print(estiek)
+
+# Feld - Összegzés + Kiválogatás
+
