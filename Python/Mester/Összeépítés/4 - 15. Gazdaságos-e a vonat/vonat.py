0f2128fa059e6b789dd59815d062576a2780bd12
farkas.norbert <farkas.norbert@ckik.hu>
2024-01-31 10:56:38 +0100

    vonat.py

--- Python/Mester/Összeépítés/4 - 15. Gazdaságos-e a vonat/vonat.py
+++ Python/Mester/Összeépítés/4 - 15. Gazdaságos-e a vonat/vonat.py
@@ -1,8 +1,8 @@
 # Be
-n = int(input())
+n = int(input()) # 6
 masodiksor = input().split()
 ar = int(masodiksor[0])
-koltseg = int(masodiksor[1])
+koltseg = int(masodiksor[1]) # 1000
 leszallok, felszallok = [], []
 for i in range(n):
     sor = input().split()
@@ -10,9 +10,26 @@ for i in range(n):
     leszallok.append(le)
     fel = int(sor[1])
     felszallok.append(fel)
-print(felszallok)
+# print(felszallok)
 
 # Feld
+bevetelek = []
+letszam = 0
+for i in range(n-1):
+    letszam = letszam + felszallok[i] - leszallok[i]
+    bevetel = letszam * ar
+    bevetelek.append(bevetel)
+# print(bevetelek)
 
+osszbevetel = 0
+for i in range(len(bevetelek)):
+    osszbevetel += bevetelek[i]
+# print(osszbevetel)
+
+osszkoltseg = (n-1) * koltseg
 
 # Ki
+if osszkoltseg < osszbevetel:
+    print(1)
+else:
+    print(0)
