9d43d8e5b7746fd57c5cb3a27030439ff02836e9
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-02 14:04:14 +0100

    matek.py

---
+++ Python/Modulok/matek.py
@@ -0,0 +1,10 @@
+# import math => math.sqrt(2)
+# import math as m => m.sqrt(2)
+# from math import sqrt, factorial, comb, gcd, pi => sqrt(2)
+from math import *
+
+print("Gyök(2) =", round(sqrt(2), 3))
+print("5! =", factorial(5))
+print("5 alatt 2 =", comb(5, 2))
+print("LNKO(120, 150) =", gcd(120, 150))
+print("Pi értéke:", round(pi, 100))
