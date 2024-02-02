0190fcaaff33e551ab360b155c88e19629676736
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-02 09:40:58 +0100

    gyokvonas, math modul

---
+++ Python/Modulok/matek.py
@@ -0,0 +1,7 @@
+# import math => math.sqrt(2)
+# import math as m => m.sqrt(2)
+# from math import sqrt, factorial
+from math import *
+
+print("Gyök(2) =", round(sqrt(2), 3))
+print("5! =", factorial(5))
