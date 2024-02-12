b6e72717be9dc730537e3dabc019696a1ca2f5c1
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-12 13:57:36 +0100

    teglalap.py

---
+++ Python/Függvények/teglalap.py
@@ -0,0 +1,24 @@
+# Téglalap oldalai: a és b
+# Terület: a*b
+# Kerület: 2*(a+b)
+
+# Matek: f(x)
+
+# Függvény: "gép", ami 
+# bemenő paraméterekből
+# visszatérési értéket csinál
+def Terulet(a, b):
+    return a * b
+
+# Kéne: Kerulet fv???
+def Kerulet(a, b):
+    return 2 * (a + b)
+
+print(Terulet(3, 5))
+print(Terulet(12, 15))
+print(Terulet(2, 7))
+
+print("-"*25)
+print(Kerulet(3, 5))
+print(Kerulet(12, 15))
+print(Kerulet(2, 7))
