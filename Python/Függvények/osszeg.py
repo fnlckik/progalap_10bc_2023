c8790ee3e73037b695a2b638023da6bec282955f
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-13 13:57:19 +0100

    osszeg (BB)

--- Python/Függvények/osszeg.py
+++ Python/Függvények/osszeg.py
@@ -1,8 +1,23 @@
 # Visszatérési értéke: elemek összeg
 # osszeg(lista)
 
+# Benedek Bence
+def osszeg(a):
+    osszeg = 0
+    for i in range(len(a)):
+        osszeg += a[i]
+    return osszeg
 
+# Kulcsár Máté
+def osszeg2(a):
+    ...
 
 print(osszeg([2, 5, -1])) #6
 print(osszeg([3, -1])) #2
 print(osszeg([1, 0, 2, 3, -1])) #5
+
+print("-"*25)
+
+print(osszeg2(2, 5, -1)) #6
+print(osszeg2(3, -1)) #2
+print(osszeg2(1, 0, 2, 3, -1)) #5
