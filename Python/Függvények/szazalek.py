5a5fb34f780273f08d53bb62ca4be222b79522c3
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-15 12:51:59 +0100

    szazalek.py

--- Python/Függvények/szazalek.py
+++ Python/Függvények/szazalek.py
@@ -1,9 +1,14 @@
 # print("2 / 5 = ", round(2 / 5 * 100), "%", sep="")
 # print("2 / 5 = " + str(round(2 / 5 * 100)) + "%")
 
-# Ez függvény?
+# Ez függvény? => "Nem"
+# "Nincs return" (visszatérési érték)
+# Ez eljárás!
+# Function => return
+# Procedure => nincs return
 def kiir(a, b):
-    print(f"{a} / {b} = {round(a / b * 100)}%")
+    szazalek = round(a / b * 100)
+    print(f"{a} / {b} = {szazalek}%")
 
 kiir(2, 5)
 kiir(2, 3)
@@ -18,3 +23,9 @@ kiir(82, 13)
 
 # "Tarts szárazon a kódod!" DRY
 # DRY: Don't repeat yourself!
+
+# Mellékhatás: 
+# egy fv csinál valami mást is a számolás mellet
+# Pl.: értéket módosít, kiír valamit...
+
+# Fv-ek legyenek mellékhatás mentesek!
