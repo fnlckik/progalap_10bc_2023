78c7daea85fdb739ead51dc506c7af6d84a50c71
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-23 13:48:40 +0100

    halmaz_muveletek

--- Python/Függvények/halmaz_muveletek.py
+++ Python/Függvények/halmaz_muveletek.py
@@ -38,6 +38,16 @@ def kulonbseg(a, b):
             eredmeny.append(a[i])
     return eredmeny
 
+def szimm_diff(a, b):
+    u = unio(a, b)
+    m = metszet(a, b)
+    return kulonbseg(u, m)
+
+def szimm_diff2(a, b):
+    k1 = kulonbseg(a, b)
+    k2 = kulonbseg(b, a)
+    return unio(k1, k2)
+
 def main():
     a = [3, 5, 2, -7, 13, 8] #6 elem
     b = [1, -4, 2, 8, 14] #5 elem
@@ -53,8 +63,22 @@ def main():
     u = unio(a, b)
     print("Unio:", u)
     
-    # Különbség: [3, 5, -7, 13]
-    k = kulonbseg(a, b)
-    print("Különbség:", k)
+    # A különbség nem szimmetrikus!
+    # A\B =/= B\A
+    # Különbség (A\B): [3, 5, -7, 13]
+    k1 = kulonbseg(a, b)
+    print("Különbség:", k1)
+    # Különbség (B\A): [1, -4, 14]
+    k1 = kulonbseg(b, a)
+    print("Különbség2:", k1)
+    
+    # Szimmetrikus differencia
+    # Azon elemek halmaza, amelyek
+    # csakis az egyik halmazban vannak benne!
+    sz = szimm_diff(a, b)
+    print("Szimmetrikus differencia:", sz)
+    
+    sz2 = szimm_diff2(a, b)
+    print("Szimmetrikus differencia:", sz2)
     
 main()
