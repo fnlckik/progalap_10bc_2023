7602455a9aba6fa54b23bd9aed81d31cb7e8d4da
farkas.norbert <farkas.norbert@ckik.hu>
2024-02-29 13:26:22 +0100

    kétjegyű szavak (vonalak)

--- Python/Gyakorlás/akasztofa.py
+++ Python/Gyakorlás/akasztofa.py
@@ -8,6 +8,10 @@ def sorsolas():
         "nem", "monitor",
         "microsoft"
     ]
+    # szavak = ["szőlő", "kutya", "macska", "szivárvány", "nyuszi"]
+    # szavak = ["tyúkszárny"]
+    # szavak = ["egészség"]
+    # szavak = ["rézszék"]
     r = randrange(len(szavak))
     return szavak[r]
 
@@ -16,20 +20,37 @@ def sorsolas():
 # Pl.: "c", "s" => True
 # Pl.: "t", "x" => False
 def eleme(c1, c2, lista):
-    ...
+    szo = c1 + c2
+    i = 0
+    while i < len(lista) and not(szo == lista[i]):
+        i += 1
+    return i < len(lista)
+    # if i < len(lista):
+    #     return True
+    # else:
+    #     return False
 
 def kiiras(szo):
     # print("_ "*len(szo))
     ketjegyuek = ["cs", "dz", "gy", "ly", "ny", "sz", "ty", "zs"]
-    for i in range(len(szo)):
-        if eleme(szo[i], szo[i+1], ketjegyuek):
+    ketjegyuE = False
+    i = 0
+    while i < len(szo) - 1:
+        ketjegyuE = eleme(szo[i], szo[i+1], ketjegyuek)
+        if ketjegyuE:
             print("__ ", end="")
+            i += 2
         else:
             print("_ ", end="")
-    print()
+            i += 1
+    if not ketjegyuE:
+        print("_")
+    else:
+        print()
 
 def main():
     megoldas = sorsolas()
+    print(megoldas)
     kiiras(megoldas)
 
 main()
