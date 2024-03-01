3ee736a334607846e5efed0a856ede83565dc4b5
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-01 14:17:54 +0100

    akasztófa - játék ciklus

--- Python/Gyakorlás/akasztofa.py
+++ Python/Gyakorlás/akasztofa.py
@@ -39,22 +39,22 @@ def eleme(c1, c2, lista):
 # Kiírja a kapott lista elemeit
 # figyelve (viszonylag) a kétjegyű
 # mássalhangzókra.
-def kiirasa(szo):
+def kiiras(aktualis, megoldas):
     # print("_ "*len(szo))
     print("Feladvány:")
     ketjegyuek = ["cs", "dz", "gy", "ly", "ny", "sz", "ty", "zs"]
     ketjegyuE = False
     i = 0
-    while i < len(szo) - 1:
-        ketjegyuE = eleme(szo[i], szo[i+1], ketjegyuek)
+    while i < len(aktualis) - 1:
+        ketjegyuE = eleme(megoldas[i], megoldas[i+1], ketjegyuek)
         if ketjegyuE:
-            print(f"{szo[i]}{szo[i+1]} ", end="")
+            print(f"{aktualis[i]}{aktualis[i+1]} ", end="")
             i += 2
         else:
-            print(f"{szo[i]} ", end="")
+            print(f"{aktualis[i]} ", end="")
             i += 1
     if not ketjegyuE:
-        print(szo[i])
+        print(aktualis[i])
     else:
         print()
 
@@ -68,37 +68,68 @@ def kezdeti_allas(megoldas):
         lista.append("_")
     return lista
 
-# Eljárás:
+# Függvény:
 # Megnézi, hogy a megoldas melyik
 # karakterei egyeznek a betu-vel
 # és az aktualis lista megfelelő
 # elemeit lecseréli a betűre.
+# Visszatérési érték: Hány csere volt?
 def csere(aktualis: list[str], betu: str, megoldas: str):
+    db = 0
     for i in range(len(megoldas)):
         if megoldas[i] == betu:
             aktualis[i] = betu
+            db += 1
+    return db
 # Példa:
 # megoldas = "alma"
 # betu = "m"
 # aktualis = ["_", "_", "_", "_"]
 # Ekkor => aktualis = ["_", "_", "m", "_"]
 
-# Eljárás:
+# Függvény:
 # Beolvasunk egy betűt a felhasználótól
 # és cseréljük a lista megfelelői
 # elemeit a helyes betűre!
+# Visszatérés: Hány találatos volt a tipp?
 def tipp(aktualis, megoldas):
     betu = input("Tipp: ")
-    csere(aktualis, betu, megoldas)
+    db = csere(aktualis, betu, megoldas)
+    return db
+
+# Függvény:
+# Egy lista karaktereit
+# szöveggé fűzi össze.
+def szoveg(lista):
+    s = ""
+    for i in range(len(lista)):
+        s += lista[i]
+    return s
+# Pl.: szoveg(["a", "l", "m", "a"]) == "alma"
+
+# Eljárás:
+# Játék ciklus
+# Ez történik a játék során.
+def jatek(aktualis, megoldas):
+    hibak_szama = 0
+    while szoveg(aktualis) != megoldas and hibak_szama < 10:
+        eltalalt = tipp(aktualis, megoldas)
+        if eltalalt == 0:
+            hibak_szama += 1
+        system("cls")
+        print("Hibák száma:", hibak_szama)
+        kiiras(aktualis, megoldas)
+    if hibak_szama < 10:
+        print("Nyertél!")
+    else:
+        print("Vesztettél!")
 
 def main():
     system("cls")
     megoldas = sorsolas()
     print(megoldas) # csalás magunk számára
     aktualis = kezdeti_allas(megoldas)
-    kiirasa(aktualis)
-    while True:
-        tipp(aktualis, megoldas)
-        kiirasa(aktualis)
-
+    kiiras(aktualis, megoldas)
+    jatek(aktualis, megoldas)
+    
 main()
