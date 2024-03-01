baa2d56d74685ff7778ed43d88220a62f05d8f27
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-01 11:41:14 +0100

    akasztofa - tipp, csere

--- Python/Gyakorlás/akasztofa.py
+++ Python/Gyakorlás/akasztofa.py
@@ -1,6 +1,10 @@
 # Csináljunk egy akasztófa játékot!
 from random import randrange
+from os import system
 
+# Függvény:
+# Választ egy véletlen szót
+# egy konstans listából!
 def sorsolas():
     szavak = [
         "krumplistészta", "algéria",
@@ -15,6 +19,7 @@ def sorsolas():
     r = randrange(len(szavak))
     return szavak[r]
 
+# Függvény:
 # Eldönti, hogy c1 és c2 karakterekből képzett szó
 # benne van-e a listában!
 # Pl.: "c", "s" => True
@@ -30,27 +35,70 @@ def eleme(c1, c2, lista):
     # else:
     #     return False
 
-def kiiras(szo):
+# Eljárás:
+# Kiírja a kapott lista elemeit
+# figyelve (viszonylag) a kétjegyű
+# mássalhangzókra.
+def kiirasa(szo):
     # print("_ "*len(szo))
+    print("Feladvány:")
     ketjegyuek = ["cs", "dz", "gy", "ly", "ny", "sz", "ty", "zs"]
     ketjegyuE = False
     i = 0
     while i < len(szo) - 1:
         ketjegyuE = eleme(szo[i], szo[i+1], ketjegyuek)
         if ketjegyuE:
-            print("__ ", end="")
+            print(f"{szo[i]}{szo[i+1]} ", end="")
             i += 2
         else:
-            print("_ ", end="")
+            print(f"{szo[i]} ", end="")
             i += 1
     if not ketjegyuE:
-        print("_")
+        print(szo[i])
     else:
         print()
 
+# Függvény:
+# Előállítja a megoldas hosszával
+# egyenlő hosszú, alulvonásokat ("_")
+# tartalmazó listát
+def kezdeti_allas(megoldas):
+    lista = []
+    for i in range(len(megoldas)):
+        lista.append("_")
+    return lista
+
+# Eljárás:
+# Megnézi, hogy a megoldas melyik
+# karakterei egyeznek a betu-vel
+# és az aktualis lista megfelelő
+# elemeit lecseréli a betűre.
+def csere(aktualis: list[str], betu: str, megoldas: str):
+    for i in range(len(megoldas)):
+        if megoldas[i] == betu:
+            aktualis[i] = betu
+# Példa:
+# megoldas = "alma"
+# betu = "m"
+# aktualis = ["_", "_", "_", "_"]
+# Ekkor => aktualis = ["_", "_", "m", "_"]
+
+# Eljárás:
+# Beolvasunk egy betűt a felhasználótól
+# és cseréljük a lista megfelelői
+# elemeit a helyes betűre!
+def tipp(aktualis, megoldas):
+    betu = input("Tipp: ")
+    csere(aktualis, betu, megoldas)
+
 def main():
+    system("cls")
     megoldas = sorsolas()
-    print(megoldas)
-    kiiras(megoldas)
+    print(megoldas) # csalás magunk számára
+    aktualis = kezdeti_allas(megoldas)
+    kiirasa(aktualis)
+    while True:
+        tipp(aktualis, megoldas)
+        kiirasa(aktualis)
 
 main()
