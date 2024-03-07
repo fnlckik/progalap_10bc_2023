f0e2f0b5f3152f3bd75afb1facc5c4a45275cd79
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-07 10:06:19 +0100

    feltolt_random

---
+++ Python/Gyakorlás/2/egyedi_hosszuak.py
@@ -0,0 +1,18 @@
+'''
+Készíts függvényt egyedi_hosszuak(x) néven, amely
+megadja az x lista azon elemeit,
+amelyek előtt nincs velük azonos hosszú elem!
+
+Paraméterek:
+x: szövegeket tartalmazó lista
+
+Visszatérési érték:
+Egy lista a megfelelő (egyedi) elemekkel!
+'''
+
+def main():
+    print(egyedi_hosszuak(["alma", "banan", "korte", "barack"]) == ["alma", "banan", "barack"])
+    print(egyedi_hosszuak(["piros", "feher", "zold", "barna", "lila"]) == ["piros", "zold"])
+    print(egyedi_hosszuak(["abc", "aab", "abba", "baba"]) == ["abc", "abba"])
+
+main()
