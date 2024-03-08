a58f0a4e4441e56732a6eba9f0339585d54db378
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-08 11:32:27 +0100

    nyaklanc.py

---
+++ Python/Gyakorlás/nyaklanc.py
@@ -0,0 +1,51 @@
+'''
+Pl.: "PSSKPSSSKKS"
+
+Sára nyakláncot készít piros, sárga és kék
+színű gyöngyökből.
+
+Egyetlen fontos szabály van:
+piros gyöngyöt nem követhet kék!
+
+Készíts függvényt nyaklanc(n) néven,
+amely visszatérési értéke egy n hosszú
+szöveg, amely P, S és K karakterekből áll
+a fenti szabálynak megfelelően!
+
+Pl.:
+nyaklanc(2) lehetséges értékei:
+"PP", "PS",
+"SS", "SK", "SP"
+"KK", "KS"
+
+Vigyázat: nem csak "PK", de "KP" sem jó,
+mert a nyaklánc két végét össze kell kötni!
+'''
+from random import randint
+
+def sorsolas(szinek):
+    # szinek = ["P", "S", "K"]
+    r = randint(0, len(szinek)-1)
+    return szinek[r]
+
+def nyaklanc(n):
+    s = sorsolas(["P", "S", "K"])
+    for i in range(1, n):
+        szin = sorsolas(["P", "S", "K"])
+        if s[i-1] == "P":
+            szin = sorsolas(["P", "S"])
+        if i == n-1 and s[0] == "K":
+            szin = sorsolas(["S", "K"])
+        if s[i-1] == "P" and i == n-1 and s[0] == "K":
+            szin = "S"
+        s += szin
+    return s
+
+# Tesztelés
+def main():
+    print(nyaklanc(2))
+    print(nyaklanc(3))
+    print(nyaklanc(7))
+    print(nyaklanc(15))
+
+main()
