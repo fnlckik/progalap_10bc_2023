d9d7dd55fa31107303168101613e9b5b6417b0cd
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-08 09:12:24 +0100

    egyedi_hosszuak.py

--- Python/Gyakorlás/2/egyedi_hosszuak.py
+++ Python/Gyakorlás/2/egyedi_hosszuak.py
@@ -10,6 +10,22 @@ Visszatérési érték:
 Egy lista a megfelelő (egyedi) elemekkel!
 '''
 
+def bennevan(elem, lista):
+    i = 0
+    while i < len(lista) and not(lista[i] == elem):
+        i += 1
+    return i < len(lista)
+
+def egyedi_hosszuak(szavak):
+    egyediek = []
+    hosszak = []
+    for i in range(len(szavak)):
+        if not bennevan(len(szavak[i]), hosszak):
+            egyediek.append(szavak[i])
+            hosszak.append(len(szavak[i]))
+    return egyediek
+
+# [4, 5, 5, 6] => [4, 5, 6]
 def main():
     print(egyedi_hosszuak(["alma", "banan", "korte", "barack"]) == ["alma", "banan", "barack"])
     print(egyedi_hosszuak(["piros", "feher", "zold", "barna", "lila"]) == ["piros", "zold"])
