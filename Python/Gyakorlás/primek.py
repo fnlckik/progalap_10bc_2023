9daca54b91b3c0822e6609c59f974627f8eee9b8
farkas.norbert <farkas.norbert@ckik.hu>
2024-03-08 11:00:33 +0100

    primek.py

--- Python/Gyakorlás/primek.py
+++ Python/Gyakorlás/primek.py
@@ -17,21 +17,30 @@ csak egyedi számok legyenek!
 # Ha van => nem prím
 # különben => prím
 def primE(n):
+    if n == 1 or n == 0:
+        return False
+    n = abs(n)
     i = 2
     while i < n and not(n % i == 0):
         i += 1
     return not(i < n)
 
+def eleme(elem, lista):
+    i = 0
+    while i < len(lista) and not(lista[i] == elem):
+        i += 1
+    return i < len(lista)
+
 def primek(szamok):
     eredmeny = []
     for i in range(len(szamok)):
-        if primE(szamok[i]):
+        if primE(szamok[i]) and not eleme(szamok[i], eredmeny):
             eredmeny.append(szamok[i])
     return eredmeny
 
 # Tesztelés
 def main():
-    print(primek([5, 1, 26, 17, 38]))
+    # print(primek([3, 11, 5, 3, 2, 3, 3, 2, 7]))
     print(primek([3, 11, 5, 3, 2, 3, 3, 2, 7]) == [3, 11, 5, 2, 7])
     print(primek([15, 101, 53, 119]) == [101, 53])
     print(primek([16, 32, 64, 128]) == [])
